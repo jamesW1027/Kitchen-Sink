09b97488d0b672b1de7944aae1d781c02e0b58fc
student <student@Admins-iMac.local>
2019-09-09 09:55:49 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+var name = "James";
+
+const numberOfUSStates = 50;
+
+
+    function addnumbers (a,b) {
+        return 5+4;
+    }
+function sayHello () {
+    alert ("hello world!")
+
+}
+sayHello ()
+//this function is meant to print out "Hello world!"
+
+var persons = [
+    charles = { name: 'Charles', age: 21},
+    abby = { name: 'Abby' , age: 27},
+    james = { name: 'James' , age: 18},
+    john = { name: 'John' , age: 17}
+]
+function checkAge (name,age) {
+    if (age< 21) {
+        alert  ("Sorry " + name + ", you aren't old enough to view this page!");
+    }
+}
+
+var vegetables = [ 'corn' , 'jalapeno' , 'potato' ]
+    if ( x=0, x<vegetables.length, x++ ) {
+        console.log (vegetables [x]); 
+    }
+
+var persons = [
+    billy = { name:'Billy' , age: 19},
+    mallory = { name: 'Mallory' , age: 23},
+    jonathan = { name: 'Jonathan' , age: 72 },
+    alex = { name: 'Alex', age: 28},
+    milton = {name: 'Milton' , age : 45}
+]
+function checkAge (name,age){
+    if (age< 21) {
+        alert ("Sorry " + name + ", you aren't old enough to view this page!");
+    } 
+}
+
+function getLength {
+    
+}
